b7ff04e2425af31a6a36e8d654b3f3f532658bb5
Dylan Wild <wilddylan@gmail.com>
2019-06-20 09:49:39 +0100

    Initial Commit

---
+++ Dice GameTests/Dice_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dice_GameTests.swift
+//  Dice GameTests
+//
+//  Created by Codenation13 on 20/06/2019.
+//  Copyright © 2019 codenation. All rights reserved.
+//
+
+import XCTest
+@testable import Dice_Game
+
+class Dice_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
